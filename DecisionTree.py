636de76a591097bba8242419e693c152162ab787
Bobur Shermatov <113255219+SHERBO9@users.noreply.github.com>
2023-04-29 11:07:12 +0500

    Test commit

--- DecisionTree.py
+++ DecisionTree.py
@@ -3,6 +3,7 @@ from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 
+# Bu Decision tree metodiga oddiy misol
 df = pandas.read_csv("DecisionTree.csv")
 
 d = {'UK': 0, 'USA': 1, 'N': 2}
